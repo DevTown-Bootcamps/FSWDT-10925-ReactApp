aff11e0cbaea90abf94da034203dd8a0a34a3064
[200~anshulojha1214@gmail.com <anshulojha1214@gmail.com>
2025-10-22 18:34:53 +0530

    react app

---
+++ src/components/classComponent.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class Greet extends Component{
+    render(){
+        return(
+            <div>
+                <h2>Hello this is Anshul</h2>
+             <p>This is a Class component</p>
+            </div>
+        )
+    }
+}
+
+export default Greet;
---
+++ src/components/functionalComponenet.jsx
@@ -0,0 +1,23 @@
+//simple js function.
+//It manages states using Hooks.
+
+//Return
+//parent tag in return
+//exporting a function- 1) exporting at definition 2)Exporting at the end.
+
+import React from "react";
+
+function Welcome(){
+    return (
+        <>
+         <div>
+             <h2>Hello this is Anshul</h2>
+             <p>This is a functional component</p>
+         </div>
+        </>
+    )
+}
+
+export default Welcome;
+
+

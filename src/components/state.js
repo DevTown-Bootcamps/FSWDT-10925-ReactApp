65e8096a11ec2f112b58f3d743a4fdf81cb31615
[200~anshulojha1214@gmail.com <anshulojha1214@gmail.com>
2025-10-23 18:39:06 +0530

    useEffect

--- src/components/state.js
+++ src/components/state.js
@@ -1,29 +1,53 @@
-import React,{ useState } from "react";
+import React,{ useState,useEffect } from "react";
 
 // 1)useState
 
-export default function Counter(){
-    const [count,setCounter] =useState(0);
+// export default function Counter(){
+//     const [count,setCounter] =useState(0);
 
-    const increment = ()=>{
-        setCounter(count+1);
-    }
+//     const increment = ()=>{
+//         setCounter(count+1);
+//     }
     
 
-    const decremet = ()=>{
-        setCounter(count-1);
-    }
+//     const decremet = ()=>{
+//         setCounter(count-1);
+//     }
 
-    return(
-        <div>
-            <h2>{count}</h2>
-            <button onClick={increment}>Increment</button>
-            <button onClick={decremet}>Decrement</button>
-        </div>
-    )
-}
+//     return(
+//         <div>
+//             <h2>{count}</h2>
+//             <button onClick={increment}>Increment</button>
+//             <button onClick={decremet}>Decrement</button>
+//         </div>
+//     )
+// }
+
+
+//             <h2>{count}</h2>
+//             <button onClick={()=> setCounter(count+1))}>Increment</button>
+//             <button onClick={()=> setCounter(count-1))}>Decrement</button>
+
 
 // count=count+2;
 // setCounter(count+1);
 
 //write a code to update the state by (count+5)/2
+
+//useEfffect-used to perform side effects(fetching data, subscription, and timmer)
+
+export default function Counter(){
+    const [count,setCounter]=useState(0);
+
+    useEffect(()=>{
+        console.log("Componenet reached");
+        return ()=> console.log("Component reached");
+    },[count]);
+
+    return (
+        <div>
+            <p>You have clicked the button {count} times</p>
+            <button onClick={()=> setCounter(count+1)}>Increment</button>
+        </div>
+    )
+}

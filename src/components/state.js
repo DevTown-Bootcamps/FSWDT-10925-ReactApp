172684ac51e4f32c5a3d6c78e5f03071a32e9a8c
[200~anshulojha1214@gmail.com <anshulojha1214@gmail.com>
2025-10-23 18:54:09 +0530

    usecontext

--- src/components/state.js
+++ src/components/state.js
@@ -1,4 +1,5 @@
-import React,{ useState,useEffect } from "react";
+import { getValue } from "@testing-library/user-event/dist/utils";
+import React,{ useState,useEffect,useContext,createContext } from "react";
 
 // 1)useState
 
@@ -36,18 +37,38 @@ import React,{ useState,useEffect } from "react";
 
 //useEfffect-used to perform side effects(fetching data, subscription, and timmer)
 
-export default function Counter(){
-    const [count,setCounter]=useState(0);
+// export default function Counter(){
+//     const [count,setCounter]=useState(0);
 
-    useEffect(()=>{
-        console.log("Componenet reached");
-        return ()=> console.log("Component reached");
-    },[count]);
+//     useEffect(()=>{
+//         console.log("Componenet reached");
+//         return ()=> console.log("Component reached");
+//     },[count]);
 
+//     return (
+//         <div>
+//             <p>You have clicked the button {count} times</p>
+//             <button onClick={()=> setCounter(count+1)}>Increment</button>
+//         </div>
+//     )
+// }
+
+
+const ThemeContext=createContext();
+
+function Context(){
     return (
-        <div>
-            <p>You have clicked the button {count} times</p>
-            <button onClick={()=> setCounter(count+1)}>Increment</button>
-        </div>
+        <ThemeContext.Provider value="dark">
+            <Toolbar/>
+        </ThemeContext.Provider>
     )
+};
+
+function Toolbar(){
+    return <ThemeButton/>;
+}
+
+function ThemeButton(){
+    const theme=useContext(ThemeContext);
+    return <button style={{background: theme==="dark"?'#333':'#fff'}}>Click me</button>
 }
